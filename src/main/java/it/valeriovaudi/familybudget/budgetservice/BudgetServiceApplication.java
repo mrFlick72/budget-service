d1955bcd31abbc4e5b8f40c134255b98577dc599
mrFlick72 <valerio.vaudi@gmail.com>
2020-04-02 18:37:41 +0200

    graphite metrics

--- src/main/java/it/valeriovaudi/familybudget/budgetservice/BudgetServiceApplication.java
+++ src/main/java/it/valeriovaudi/familybudget/budgetservice/BudgetServiceApplication.java
@@ -1,7 +1,11 @@
 package it.valeriovaudi.familybudget.budgetservice;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BudgetServiceApplication {
@@ -9,4 +13,10 @@ public class BudgetServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BudgetServiceApplication.class, args);
     }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name:}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
 }
