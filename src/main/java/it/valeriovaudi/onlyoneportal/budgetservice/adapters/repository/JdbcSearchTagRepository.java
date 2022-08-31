0fd2215ce2cf2b910f8d62b738034f9f350ff363
mrflick72 <valerio.vaudi@gmail.com>
2022-09-01 00:11:10 +0200

    version updates query issue fix

--- src/main/java/it/valeriovaudi/onlyoneportal/budgetservice/adapters/repository/JdbcSearchTagRepository.java
+++ src/main/java/it/valeriovaudi/onlyoneportal/budgetservice/adapters/repository/JdbcSearchTagRepository.java
@@ -22,22 +22,22 @@ public class JdbcSearchTagRepository implements SearchTagRepository {
 
     @Override
     public SearchTag findSearchTagBy(String key) {
-        return jdbcTemplate.queryForObject("SELECT * FROM SEARCH_TAG WHERE KEY=? AND USER_NAME=?", new Object[]{key, userRepository.currentLoggedUserName()},
+        return jdbcTemplate.queryForObject("SELECT * FROM SEARCH_TAG WHERE KEY=? AND USER_NAME=?", new String[]{key, userRepository.currentLoggedUserName().getContent()},
                 (rs, rowNum) -> new SearchTag(rs.getString("KEY"), rs.getString("VALUE")));
     }
 
     @Override
     public List<SearchTag> findAllSearchTag() {
-        return jdbcTemplate.query("SELECT * FROM SEARCH_TAG WHERE USER_NAME=?", new Object[]{userRepository.currentLoggedUserName()}, (rs, rowNum) -> new SearchTag(rs.getString("KEY"), rs.getString("VALUE")));
+        return jdbcTemplate.query("SELECT * FROM SEARCH_TAG WHERE USER_NAME=?", new String[]{userRepository.currentLoggedUserName().getContent()}, (rs, rowNum) -> new SearchTag(rs.getString("KEY"), rs.getString("VALUE")));
     }
 
         @Override
         public void save (SearchTag searchTag){
-            jdbcTemplate.update("INSERT INTO SEARCH_TAG(KEY, VALUE, USER_NAME) VALUES (?, ?, ?) ON CONFLICT (KEY) DO UPDATE SET VALUE=?", searchTag.getKey(), searchTag.getValue(), searchTag.getValue());
+            jdbcTemplate.update("INSERT INTO SEARCH_TAG(KEY, VALUE, USER_NAME) VALUES (?, ?, ?) ON CONFLICT (KEY) DO UPDATE SET VALUE=?", searchTag.getKey(), searchTag.getValue(), userRepository.currentLoggedUserName().getContent(), searchTag.getValue());
         }
 
         @Override
         public void delete (String key){
-            jdbcTemplate.update("DELETE FROM SEARCH_TAG WHERE KEY=? AND USER_NAME=?", key, userRepository.currentLoggedUserName());
+            jdbcTemplate.update("DELETE FROM SEARCH_TAG WHERE KEY=? AND USER_NAME=?", key, userRepository.currentLoggedUserName().getContent());
         }
     }
