e4a66064114264849e252d61c3dcc1b83ccd0867
mrflick72 <valerio.vaudi@gmail.com>
2022-09-10 01:03:55 +0200

    key design fix

--- src/main/java/it/valeriovaudi/onlyoneportal/budgetservice/adapters/repository/DynamoDbIdProvider.java
+++ src/main/java/it/valeriovaudi/onlyoneportal/budgetservice/adapters/repository/DynamoDbIdProvider.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public class DynamoDbIdProvider implements IdProvider {
 
     public String id() {
-        return UUID.randomUUID().toString();
+        return null;
     }
 
     public BudgetExpenseId newBudgetExpenseId() {
